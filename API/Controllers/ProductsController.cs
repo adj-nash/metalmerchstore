1ec68251b58d932b2a0672160467ab8c24f54057
adj-nash <168261681+adj-nash@users.noreply.github.com>
2025-01-03 14:52:37 +0000

    Project commit.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext context) : ControllerBase
+    {
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetAllProducts()
+    {
+        return await context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+
+        if(product == null) return NotFound("There was a problem finding this product!");
+
+        return product;
+    }
+
+    }
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,17 @@
+namespace API.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public long Price { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string Category { get; set; }
+    public int Stock { get; set; }
+    public char Size { get; set; }
+    public required string Band { get; set; }
+    public required string Genre { get; set; }
+
+
+}
