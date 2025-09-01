e46e952f384d94e21864ae4b356111cc0c0751b4
adj-nash <168261681+adj-nash@users.noreply.github.com>
2025-09-01 18:24:40 +0100

    Revert "Fix"
    This reverts commit a58d79de035bbf8604d47ff6ae7e5b5558207b36.

--- client/src/features/about/About.tsx
+++ client/src/features/about/About.tsx
@@ -11,7 +11,7 @@ export default function About() {
                 "from my own personal collection of over 300+ band merch items which I have collected over the years. I’ve been inspired by " +
                 "similar sites such as Impericon, EMP, Damaged Society etc.\n\n Feel free to browse each page, try out the various filters on the" + 
                 "merchandise page and even sign up! Of course this website is a work in progress and I’m currently working on new features which I " +
-                "intend to implement over the coming weeks..\n\n";
+                "intend to implement over the coming weeks.\n\n";
 
   return (
 
